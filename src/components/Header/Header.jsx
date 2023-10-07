3ba39b680aa0f7789763b879cd47f161c22eceee
JungHoWoon <jhu940810@gmail.com>
2023-10-07 18:37:04 +0900

    Feat: darkmode implement

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './Header.module.css';
+import { DarkModeContext } from '../../context/DarkModeContext';
+import { BiSolidMoon, BiSolidSun } from 'react-icons/bi';
 
 export default function Header({ filters, filter, onChange }) {
+  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
   return (
     <header className={styles.header}>
+      <button className={styles.toggle} onClick={toggleDarkMode}>
+        {darkMode ? <BiSolidSun /> : <BiSolidMoon />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
